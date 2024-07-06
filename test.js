0d3e4a9512bae1051b0fcaea880b74838f4eeed9
Praise <126498365+halellight@users.noreply.github.com>
2024-07-07 00:46:57 +0100

    Add files via upload
    flowers

---
+++ test.js
@@ -0,0 +1,15 @@
+<<<<<<< HEAD
+onload = () => {
+  const c = setTimeout(() => {
+    document.body.classList.remove("not-loaded");
+    clearTimeout(c);
+  }, 1000);
+};
+=======
+onload = () => {
+  const c = setTimeout(() => {
+    document.body.classList.remove("not-loaded");
+    clearTimeout(c);
+  }, 1000);
+};
+>>>>>>> db4fe693e3df3c71fb608f710a5c8f297ec5fa42
